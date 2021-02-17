a049509d4dbb43d4783d71cf9c63f335241f669a
Nobi <nobi@nobidev.com>
2021-02-17 16:48:43 +0700

    Add icon view

---
+++ src/Views/icons/preset.blade.php
@@ -0,0 +1,7 @@
+@php /** @var string $namespace */ @endphp
+
+@include($namespace . '::icons.icon_check_failed')
+@include($namespace . '::icons.icon_check_success')
+@include($namespace . '::icons.icon_next_step')
+@include($namespace . '::icons.icon_retry')
+@include($namespace . '::icons.icon_error')
