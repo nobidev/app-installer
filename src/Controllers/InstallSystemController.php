ffbdab659c8161c254e96a3a365200e5f6becece
Nobi <nobi@nobidev.com>
2021-02-24 21:42:50 +0700

    fixup! fixup! Add system page

--- src/Controllers/InstallSystemController.php
+++ src/Controllers/InstallSystemController.php
@@ -20,6 +20,26 @@ class InstallSystemController extends InstallController
 {
     protected bool $allow_next = false;
 
+    public function getContextData(Request $request): array
+    {
+        $result = SystemHelper::getResult();
+        $allow_next = $this->allow_next;
+        $locales = [];
+        foreach (ResourceBundle::getLocales('') as $locale) {
+            $language = locale_get_display_language($locale, app()->getLocale());
+            $region = locale_get_display_region($locale, app()->getLocale());
+            if ($region) {
+                continue;
+            }
+            $locales[$locale] = $language;
+        }
+
+        return array_merge(
+            parent::getContextData($request),
+            compact('result', 'allow_next', 'locales'),
+        );
+    }
+
     protected function setState(array $data): void
     {
         parent::setState($data);
@@ -42,26 +62,6 @@ class InstallSystemController extends InstallController
         }
         EnvManager::save();
         $this->allow_next = Artisan::call('key:generate --ansi') === 0;
-    }
-
-    public function getContextData(Request $request): array
-    {
-        $result = SystemHelper::getResult();
-        $allow_next = $this->allow_next;
-        $locales = [];
-        foreach (ResourceBundle::getLocales('') as $locale) {
-            $language = locale_get_display_language($locale, app()->getLocale());
-            $region = locale_get_display_region($locale, app()->getLocale());
-            if ($region) {
-                continue;
-            }
-            $locales[$locale] = $language;
-        }
-
-        return array_merge(
-            parent::getContextData($request),
-            compact('result', 'allow_next', 'locales'),
-        );
     }
 
     protected function getView(): ?string
